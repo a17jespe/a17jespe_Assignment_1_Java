4f58b36a63c044c097cb2ed718a7862fd2faacfb
a17jespe <a17jespe@student.his.se>
2021-03-31 08:54:04 +0200

    Same as first but Java

---
+++ app/src/main/java/com/example/a17jespe_assignment_1_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a17jespe_assignment_1_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
